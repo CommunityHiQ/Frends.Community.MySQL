2da825b7dd05ca2a19ba03dba7512645a5af93bf
Jarkko Koivunen <jarkko.koivunen@hiq.fi>
2019-05-20 10:26:49 +0300

    First version of MySqlQuery Task

---
+++ Frends.Community.MySql/QueryTask.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MySql;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Frends.Community.MySql
+{
+    /// <summary>
+    /// Task for performing queries in MySql databases. See documentation at https://github.com/CommunityHiQ/Frends.Community.MySQL
+    /// </summary>
+    public class QueryTask
+    {
+        /// <summary>
+        /// Task for performing queries in MySql databases. See documentation at https://github.com/CommunityHiQ/Frends.Community.MySQL
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="output"></param>
+        /// <param name="connection"></param>
+        /// <param name="options"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Object { bool Success, string Message, string Result }</returns>
+        public static async Task<Output> Query(
+            [PropertyTab] QueryProperties query,
+            [PropertyTab] OutputProperties output,
+            [PropertyTab] ConnectionProperties connection,
+            [PropertyTab] Options options,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                using (var c = new MySqlConnection(connection.ConnectionString))
+                {
+                    try
+                    {
+                        await c.OpenAsync(cancellationToken);
+
+                        using (var command = new MySqlCommand(query.Query, c))
+                        {
+                            command.CommandTimeout = connection.TimeoutSeconds;
+                            
+                            // check for command parameters and set them
+                            if (query.Parameters != null)
+                                command.Parameters.AddRange(query.Parameters.Select(p => CreateMySqlParameter(p)).ToArray());
+
+                            // declare Result object
+                            string queryResult;
+
+                            // set commandType according to ReturnType
+                            switch (output.ReturnType)
+                            {
+                                case QueryReturnType.Xml:
+                                    queryResult = await command.ToXmlAsync(output, cancellationToken);
+                                    break;
+                                case QueryReturnType.Json:
+                                    queryResult = await command.ToJsonAsync(output, cancellationToken);
+                                    break;
+                                case QueryReturnType.Csv:
+                                    queryResult = await command.ToCsvAsync(output, cancellationToken);
+                                    break;
+                                default:
+                                    throw new ArgumentException("Task 'Return Type' was invalid! Check task properties.");
+                            }
+
+                            return new Output { Success = true, Result = queryResult };
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                    finally
+                    {
+                        // Close connection
+                        c.Dispose();
+                        c.Close();
+                        MySqlConnection.ClearPool(c);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (options.ThrowErrorOnFailure)
+                    throw ex;
+                return new Output
+                {
+                    Success = false,
+                    Message = ex.Message
+                };
+            }
+        }
+        /// <summary>
+        /// Gets list of parameters available for MySql.
+        /// </summary>
+        public static MySqlParameter CreateMySqlParameter(QueryParameter parameter)
+        {
+            return new MySqlParameter()
+            {
+                ParameterName = parameter.Name,
+                Value = parameter.Value,
+                MySqlDbType = parameter.DataType.ConvertEnum<MySqlDbType>()
+            };
+        }
+    }
+}
